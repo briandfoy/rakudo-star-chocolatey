bd86178ae4acad58dc7e61b9e880a93980f8be67
brian d foy <brian.d.foy@gmail.com>
2017-05-03 12:29:45 -0400

    Update to 2017.04.1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName= 'rakudostar'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://rakudo.perl6.org/downloads/star/rakudo-star-2017.04-x86_64%20(JIT).msi'
+$url64      = 'https://rakudo.perl6.org/downloads/star/rakudo-star-2017.04.1-x86_64%20(JIT).msi'
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
@@ -11,7 +11,7 @@ $packageArgs = @{
 
   softwareName  = 'rakudo*'
 
-  checksum64    = '702346fe0cb7338f57869c6f92fc6589f2af88fc2470a06c2fcaedfdb55c2e70'
+  checksum64    = '6654495651890ebcee98c0d33d392cdcdc18d8deb0412f6f7026cb730bf0bbed'
   checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
